967dc77df6b074ce7ecee11f5ff98394abf3dc25
jzhou <jzhou23@stevens.edu>
2017-10-13 15:44:27 -0400

    set up Auth reducerr with an action

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducer';
+import LoginForm from './components/LoginForm';
+
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyDeVDoEYSe4PR8vZ8AL9WPjVb35V3xK0rk',
+      authDomain: 'manager-77c3a.firebaseapp.com',
+      databaseURL: 'https://manager-77c3a.firebaseio.com',
+      projectId: 'manager-77c3a',
+      storageBucket: 'manager-77c3a.appspot.com',
+      messagingSenderId: '708719472344'
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
-      <Provider store={createStore()}>
-        <View>
-          <Text>
-            Hello World!
-          </Text>
-        </View>
+      <Provider store={createStore(reducers)}>
+        <LoginForm />
       </Provider>
     );
   }
