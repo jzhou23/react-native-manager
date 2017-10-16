6b2a06cbbb04396fc07c70ef84735cbcbfbcc171
jzhou <jzhou23@stevens.edu>
2017-10-16 16:35:57 -0400

    set up Create Employee form

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
-import { employeeUpdate } from '../actions';
+import { employeeUpdate, employeeCreate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
+  onButtonPress() {
+    const { name, phone, shift } = this.props;
+
+    this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
+  }
+
   render() {
     return (
       <Card>
@@ -25,8 +32,24 @@ class EmployeeCreate extends Component {
           />
         </CardSection>
 
+        <CardSection style={{ flexDirection: 'row' }}>
+          <Text style={styles.picketTextStyle}>Shift</Text>
+          <Picker
+            style={{ flex: 1 }}
+            selectedValue={this.props.shift}
+            onValueChange={value => this.props.employeeUpdate({ prop: 'shift', value })}
+          >
+            <Picker.Item label="Monday" value="Monday" />
+            <Picker.Item label="Tuesday" value="Tuesday" />
+            <Picker.Item label="Wednesday" value="Wednesday" />
+            <Picker.Item label="Thursday" value="Thursday" />
+            <Picker.Item label="Saturday" value="Saturday" />
+            <Picker.Item label="Sunday" value="Sunday" />
+          </Picker>
+        </CardSection>
+
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Create
           </Button>
         </CardSection>
@@ -36,10 +59,17 @@ class EmployeeCreate extends Component {
   }
 }
 
+const styles = {
+  picketTextStyle: {
+    fontSize: 18,
+    paddingLeft: 20
+  }
+};
+
 const mapStateToProps = (state) => {
   const { name, phone, shift } = state.employeeForm;
 
   return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
+export default connect(mapStateToProps, { employeeUpdate, employeeCreate })(EmployeeCreate);
